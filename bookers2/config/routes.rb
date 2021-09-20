a442ecd0db6d636a29c2ba0ce945040348ea5f59
siuhei0814 <thewindforest.0814@gmail.com>
2021-09-20 01:15:52 +0000

    2109201018

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   #検索機能
   post '/search' => 'searchs#search', as: 'search'
 
-  #グループ機能です-ーー
+  #グループ機能です-ーー-
   resources :groups do
     resources :group_users, only:[:create,:destroy]
     resources :mails, only:[:new, :create, :show]
